dc5e84d9aa22298307e836df447bc02ec5f8ebbb
Hawra Abdulla <hmabdull@qatar.cmu.edu>
2012-03-20 10:24:19 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HawraProject3::Application.config.secret_token = 'a1164b1378c929df6a1e155db24968c70f97e103e0e54ea715ce0b1a2f607a69fe7a775e70b82515ecda6334cd2238b384b0b2e6daca0adbdbc9f1b8893df146'
